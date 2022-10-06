fbc1f4c4e2a9ff230442fb61084f4fdf4487a205
tbalint11 <balintxturzo@gmail.com>
2022-10-06 21:05:25 +0200

    Users file missing error fix:(

---
+++ UserMaintenance/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UserMaintenance.Entities
+{
+    public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FullName { get; set; }
+
+    }
+}
