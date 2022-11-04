1dd1b154cb1ac9a5660e5675430f6a3c315d02e0
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-11-04 09:15:54 +0100

    8

---
+++ week06/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week06
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
