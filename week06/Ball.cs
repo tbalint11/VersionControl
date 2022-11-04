d02225ebc820bb509f4398754609b159655ba9c4
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-11-04 09:11:03 +0100

    7

--- week06/Ball.cs
+++ week06/Ball.cs
@@ -9,25 +9,13 @@ using System.Windows.Forms;
 
 namespace week06
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
+        
 
         public void MoveBall()
         {
