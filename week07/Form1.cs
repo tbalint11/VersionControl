a4470e3c4a80843511ae6b38f5055c0f44e74275
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-11-11 19:01:54 +0100

    1

--- week07/Form1.cs
+++ week07/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.Entities;
 
 namespace week07
+
 {
+    List<PortfolioItem> Portfolio = new List<PortfolioItem>();
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
