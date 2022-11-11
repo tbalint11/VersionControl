c28552c33325018b8bbaf1da714ab9843890a7cd
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-11-11 19:08:09 +0100

    8

--- week07/Form1.cs
+++ week07/Form1.cs
@@ -12,12 +12,12 @@ using week07.Entities;
 namespace week07
 
 {
-    List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+    
     public partial class Form1 : Form
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
-
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
@@ -46,7 +46,26 @@ namespace week07
                             .First();
                 value += (decimal)last.Price * item.Volume;
             }
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
             return value;
         }
+
     }
 }
