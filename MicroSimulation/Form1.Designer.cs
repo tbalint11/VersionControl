6cb4bdc2e10d9572e227c078c6d9a41c42a955c3
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-11-28 22:09:49 +0100

    fix

--- MicroSimulation/Form1.Designer.cs
+++ MicroSimulation/Form1.Designer.cs
@@ -83,7 +83,7 @@
             this.btnStart.TabIndex = 4;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.button2_Click);
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // textBox1
             // 
