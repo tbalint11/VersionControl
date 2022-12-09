ff40ab6f39bc74e6e65239ed19002fc66e73eb16
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-12-09 20:57:53 +0100

    uj gen

--- WorldsHardesGame/Form1.cs
+++ WorldsHardesGame/Form1.cs
@@ -19,6 +19,7 @@ namespace WorldsHardesGame
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        
         public Form1()
         {
             InitializeComponent();
@@ -37,11 +38,11 @@ namespace WorldsHardesGame
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
-
             var playerList = from p in gc.GetCurrentPlayers()
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
 
 
         }
@@ -52,6 +53,22 @@ namespace WorldsHardesGame
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
