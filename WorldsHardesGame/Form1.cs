bcc0786e9c4f222f6b68214c7167cdf2da665f38
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2022-12-09 20:54:15 +0100

    populacio plusz iteracios felép

--- WorldsHardesGame/Form1.cs
+++ WorldsHardesGame/Form1.cs
@@ -15,13 +15,43 @@ namespace WorldsHardesGame
     {
         GameController gc = new GameController();
         GameArea ga;
-
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            gc.AddPlayer();
+            gc.Start(true);
+
+
+            
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
